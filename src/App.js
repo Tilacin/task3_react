6a07e510218754ac5bd098f6ce50dedf7d2569bd
Tilacin <tilacin45@gmail.com>
2023-07-19 21:17:46 +0300

    Сделали управляемый select

--- src/App.js
+++ src/App.js
@@ -28,12 +28,20 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>Что тебе нужно для 😍 поездки?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
